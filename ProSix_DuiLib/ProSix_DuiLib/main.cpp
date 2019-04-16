d90c1f170ff459944989c669227a35c2172126ed
1457830962 <1457830962@qq.com>
2019-04-16 08:53:22 +0800

    Push six project

---
+++ ProSix_DuiLib/ProSix_DuiLib/main.cpp
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <UIlib.h>
+using namespace DuiLib;
+
+#ifdef _DEBUG
+#   ifdef _UNICODE
+#       pragma comment(lib, "DuiLib_ud.lib")
+#   else
+#       pragma comment(lib, "DuiLib_d.lib")
+#   endif
+#else
+#   ifdef _UNICODE
+#       pragma comment(lib, "DuiLib_u.lib")
+#   else
+#       pragma comment(lib, "DuiLib.lib")
+#   endif
+#endif
+
+class CDuiFrameWnd : public WindowImplBase
+{
+public:
+	virtual LPCTSTR    GetWindowClassName() const   { return _T("DUIMainFrame"); }
+	virtual CDuiString GetSkinFile()                { return _T("UISkin20190414.xml"); }//可以调用不同的XML文件  改变界面风格  本工程设计了两种界面风格  详见资源文件  
+	virtual CDuiString GetSkinFolder()              { return _T(""); }
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.CenterWindow();//窗口启动设置居中
+	duiFrame.ShowModal();
+	return 0;
+}
